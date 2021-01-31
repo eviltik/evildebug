9ef4c95ceacfdd673a9a52506f19e75625e465cc
eviltik <eviltik@gmail.com>
2021-01-31 15:30:22 +0100

    fix test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 const d = (() => {
-    console.log('kjqhsdkjqhsdkhqsd');
+    process.stdout.write('kjqhsdkjqhsdkhqsd');
     const projectPath = require.main.path || (process.mainModule && process.mainModule.path);
-    console.log('projectPath', projectPath);
+    process.stdout.write('projectPath='+projectPath);
     const projectName = require(path.resolve(projectPath, 'package.json')).name;
     const fileName = path.basename(require.main.filename).replace(/\.js.?$/, '');
     const debug = require('debug')(`${projectName}:${fileName}`);

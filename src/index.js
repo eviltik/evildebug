807a8334b2ac5470ade7369e18383606f9cfebd6
eviltik <eviltik@gmail.com>
2021-01-31 15:39:47 +0100

    fix test

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const path = require('path');
 const semver = require('semver');
 
 const d = (() => {
-    let projectPath;
-    if (semver.lt(process.version, '11.0.0')) {
+    let projectPath = require.main.path;
+
+    /*
+    if (semver.lt(process.version, '12.0.0')) {
         projectPath = process.mainModule.path;
     } else {
         projectPath = require.main.path;
     }
+    */
     projectPath = require.main.path || (process.mainModule && process.mainModule.path);
     process.stdout.write('projectPath='+projectPath);
     const projectName = require(path.resolve(projectPath, 'package.json')).name;
